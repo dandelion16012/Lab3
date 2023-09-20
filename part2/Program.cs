f73fb082cd6a5c5885b2ed60359daa6c44f077b4
Dandelion16012 <shchekinapa@yandex.ru>
2023-09-21 01:28:54 +0300

    3 часть

--- part2/Program.cs
+++ part2/Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Drawing;
 using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 
 class CarsCatalog
 {
-    Car[] cars;
-    public CarsCatalog (Car[] cars)
+    public Car[] cars;
+    public CarsCatalog(Car[] cars)
     {
         for (int i = 0; i < cars.Length; i++)
         {
             this.cars[i] = cars[i];
         }
     }
-    
+
+    public int this[int index]
+    {
+        get { return cars[index].getinf(); }
+
+    }
 }
-public class Car
+public class Car : IEquatable<Car>
 {
     string Name;
     string Engine;
@@ -24,18 +34,28 @@ public class Car
         this .Engine = Engine;
         this .MaxSpeed = MaxSpeed;
     }
+    public bool Equals(Car? car)
+    {
+        return (this.Name, this.Engine, this.MaxSpeed) ==
+            (car?.Name, car?.Engine, car?.MaxSpeed);
+    }
 
 
     public override string ToString()
     {
         return "Car: " + Name ;
     }
+    public int getinf()
+    {
+        Console.WriteLine($"Name\t {Name}\t Engine{Engine}");
+        return 0;
+    }
 }
 
 
 
 
-namespace part1
+namespace part2
 {
     class program
     {
@@ -44,6 +64,17 @@ namespace part1
 
             Car car = new Car ("Mystang", "Дизель", 240 );
             Console.WriteLine(car);
+            Car carOne = new Car("BMW", "Бензин",300);
+            Car carTwo = new Car("BMW", "Бензин", 280);
+            Car carThree = new Car("BMW", "Бензин", 280);
+            Car[] ar = { carOne, carTwo, carThree };
+
+            Console.WriteLine(carOne.Equals(carTwo));
+            Console.WriteLine(carThree.Equals(carTwo));
+            CarsCatalog arr = new CarsCatalog(ar);
+            arr[2];
+
+            Console.ReadKey();
 
         }
     }
--- part3/Program.cs
+++ part3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class CurrencyUSD : Currency
+{
+    public CurrencyUSD(int value) : base(value)
+    {
+    }
+}
+class CurrencyEUR : Currency
+{
+    public CurrencyEUR(int value) : base(value)
+    {
+    }
+}
+
+class CurrencyRUB : Currency
+{
+    public CurrencyRUB(int value) : base(value)
+    {
+    }
+}
+public class Currency
+{
+    int Value;
+    public Currency(int value) { this.Value = value; }
+}
+
+
+
+namespace part3
+{
+    class program
+{
+    static void Main(string[] args)
+    {
+
+        Console.ReadKey();
+
+    }
+}
+}
