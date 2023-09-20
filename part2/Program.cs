a54e2cd4daac2b848cf6a849f92ecbf1e0e535c8
Dandelion16012 <shchekinapa@yandex.ru>
2023-09-20 20:12:58 +0300

    пеализован тустринг для второй части

--- part2/Program.cs
+++ part2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+
+class CarsCatalog
+{
+    Car[] cars;
+    public CarsCatalog (Car[] cars)
+    {
+        for (int i = 0; i < cars.Length; i++)
+        {
+            this.cars[i] = cars[i];
+        }
+    }
+    
+}
+public class Car
+{
+    string Name;
+    string Engine;
+    int MaxSpeed;
+    public Car (string Name,string Engine, int MaxSpeed)
+    {
+        this .Name = Name;
+        this .Engine = Engine;
+        this .MaxSpeed = MaxSpeed;
+    }
+
+
+    public override string ToString()
+    {
+        return "Car: " + Name ;
+    }
+}
+
+
+
+
+namespace part1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car ("Mystang", "Дизель", 240 );
+            Console.WriteLine(car);
+
+        }
+    }
+}
