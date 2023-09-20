9f6e9bbb76bdb6eae1af4a5c3789bcfc368a15c6
Dandelion16012 <shchekinapa@yandex.ru>
2023-09-20 19:49:46 +0300

    первая часть полностью сделана

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Drawing;
+using System.Numerics;
 using System.Reflection.Metadata;
 
 
 public struct Vector
 {
-    int x;
-    int y;
-    int z;
-    Vector(int x, int y, int z)
+    public int x;
+    public int y;
+    public int z;
+    public Vector(int x, int y, int z)
     {
         this.x = x;
         this.y = y;
@@ -51,7 +52,16 @@ namespace part1
     {
         static void Main(string[] args)
         {
-
+            Vector v1 = new Vector(2,3,4);
+            Vector v2 = new Vector(5,6,7);
+            Vector v3= v1+ v2;
+            Console.WriteLine($"v3:\tx={v3.x}\ty={v3.y}\tz={v3.z}\t");
+            Vector v4= v1* v2;
+            Console.WriteLine($"v4:\tx={v4.x}\ty={v4.y}\tz={v4.z}\t");
+            Vector v5= 5*v1;
+            Console.WriteLine($"v5:\tx={v5.x}\ty={v5.y}\tz={v5.z}\t");
+            if (v5 == v1) Console.WriteLine("Это правда");
+            Console.WriteLine("Это ложь");
         }
     }
 }
