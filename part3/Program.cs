0b1d6866e9aa64b37da4fa235fddc17fc895c79a
Dandelion16012 <shchekinapa@yandex.ru>
2023-09-21 11:43:35 +0300

    недоделано преобразование типов

--- part3/Program.cs
+++ part3/Program.cs
@@ -1,28 +1,66 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
+class CurrencyRUB : Currency
+{
+    public CurrencyRUB(int value, double cent) : base(value, cent)
+    {
+    }
+}
 class CurrencyUSD : Currency
 {
-    public CurrencyUSD(int value) : base(value)
+    public CurrencyUSD(int value, double cent) : base(value, cent)
     {
+        
+    }
+
+    public static explicit operator CurrencyRUB(CurrencyUSD us)
+    {
+        return new CurrencyRUB { cent = us.ConvertInRub() };
     }
 }
 class CurrencyEUR : Currency
 {
-    public CurrencyEUR(int value) : base(value)
+    public CurrencyEUR(int value, double cent) : base(value, cent)
     {
     }
 }
 
-class CurrencyRUB : Currency
+
+public abstract class Currency
 {
-    public CurrencyRUB(int value) : base(value)
+    public double cent;
+    public double value;
+    double amount_in_rub;
+    //double amount_in_eur;
+    //double amount_in_usd;
+    public Currency(double value, double cent)
     {
+        this.cent = cent;
+        this.value = value;
     }
-}
-public class Currency
-{
-    int Value;
-    public Currency(int value) { this.Value = value; }
+
+    public double ConvertInRub()
+    {
+        amount_in_rub = cent * value;
+        Console.WriteLine($"Перевод в рубли:{amount_in_rub}");
+        return amount_in_rub;
+    }
+
+   
+    /*public double ConvertInEur(double amount, double value)
+    {
+        amount_in_rub = amount * value;
+        Console.WriteLine($"Перевод в евро:{amount_in_eur}");
+        return amount_in_eur;
+    }
+    public double ConvertInUsd(double amount, double value)
+    {
+        amount_in_rub = amount * value;
+        Console.WriteLine($"Перевод в долары:{amount_in_usd}");
+        return amount_in_usd;
+    }*/
+
 }
 
 
@@ -33,6 +71,10 @@ namespace part3
 {
     static void Main(string[] args)
     {
+            CurrencyEUR eu = new CurrencyEUR(100, 84.5);
+            eu.ConvertInRub();
+
+
 
         Console.ReadKey();
 
